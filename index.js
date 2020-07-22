9ee93ccea353462cb6f1caba0c8d1f6dfb28412d
maryvir <maryvirabian@gmail.com>
2020-07-22 17:50:40 +0400

    added homework

---
+++ index.js
@@ -0,0 +1,47 @@
+//factorial
+
+
+// const factorial = n => {
+//     let a = 1;
+//     for (let i = 1; i<=n; i++){
+//       a *= i;
+//     }
+//     return a;
+//   };
+//   console.log(factorial(7));
+
+
+
+// average arr
+
+// const arr = [10, 8, 56, 44];
+// function avg(arr) {
+//     let sum = 0;
+//     for(i = 0; i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//     return sum / arr.length;
+// }
+// console.log(avg(arr));
+
+
+
+// *
+// **
+// ***
+// ****
+
+
+// const stars = 7;
+// function layout(stars) {   
+//     let a = "";
+//     for (let i = 0; i < stars; i++){
+//         for (let j = 0; j<=i; j++){
+//             a += "*";
+//         }
+//         a += "\n";
+//     }
+//     return a;
+// };
+// console.log(layout(stars));
+
